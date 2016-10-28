fc596eb6a4b11594a8fc65ba6622254cac8888f2
samuel shaner <samuelshaner@gmail.com>
2016-10-28 01:40:06 +0000

    changed from 13 to 12 digits of precision in element expand

--- openmc/element.py
+++ openmc/element.py
@@ -253,7 +253,7 @@ class Element(object):
         for nuclide, abundance in abundances.items():
             nuc = openmc.Nuclide(nuclide)
             nuc.scattering = self.scattering
-            pct = float('{:.13f}'.format(percent*abundance))
+            pct = float('{:.12f}'.format(percent*abundance))
             isotopes.append((nuc, pct, percent_type))
 
         return isotopes
